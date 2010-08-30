9893d5c14690f84e5372f510c1451e316a09993e
Dave South <dave@neotribune.com>
2010-08-30 16:56:36 -0600

    Added root route.

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,4 @@ class Story
   include Mongoid::Document
   field :title
   field :body
-  field :start_at, :type => Time
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MongoidCarrierwaveUploadify::Application.routes.draw do
   resources :stories
+  root :to => 'stories#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
