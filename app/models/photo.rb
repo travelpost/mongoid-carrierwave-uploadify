8e6dc5905a3e9419174fed3fc9d23d4727bd9280
Dave South <dave@neotribune.com>
2010-08-30 17:57:21 -0600

    Added image_uploader and mounted it to photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,11 @@
+require 'carrierwave/orm/mongoid'
+
 class Photo
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :title
   embedded_in :story, :inverse_of => :photos
+
+  # CarrierWave
+  mount_uploader :image, ImageUploader
 end
