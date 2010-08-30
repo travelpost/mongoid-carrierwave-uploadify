281f2856cb817a6ecf3bda8d210e0d47e7e3d0e3
Dave South <dave@neotribune.com>
2010-08-30 15:58:12 -0600

    Initial Rails 3.0.0 repository without active_record or test_unit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidCarrierwaveUploadify::Application.config.secret_token = '26adc7bd72d8b5e5a3ac5b52e14b7384e9365d32ea8813494bd15212ac8c88fd83860481307b646b9f19e177a679a063489aaf987469c197d578a1cb67fcacdb'
