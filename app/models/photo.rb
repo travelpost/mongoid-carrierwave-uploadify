836510497c110c7be281bedfec5303c439f6d7ae
Dave South <dave@neotribune.com>
2010-09-08 15:42:17 -0600

    Save photo dimensions and orientation.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,16 +6,28 @@ class Photo
   field :title
   field :width,  :type => Integer
   field :height, :type => Integer
+  field :orientation
   embedded_in :story, :inverse_of => :photos
 
   # CarrierWave
   mount_uploader :image, ImageUploader
 
-  # before_create :save_dimensions
+  # Really no point if we don't have an image so we always require one
+  validates_presence_of :image
+
+  before_validation :save_dimensions, :save_orientation
 
 private
-  # def save_dimensions
-  #   self.width  = MiniMagick::Image.from_file(image.path)[:width]
-  #   self.height = MiniMagick::Image.from_file(image.path)[:height]
-  # end
+  def save_dimensions
+    if image.path
+      self.width  = MiniMagick::Image.from_file(image.path)[:width]
+      self.height = MiniMagick::Image.from_file(image.path)[:height]
+    end
+  end
+
+  def save_orientation
+    if image.path
+      self.orientation = (height.to_i > width.to_i) ? 'portrait' : 'landscape'
+    end
+  end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,28 +1,64 @@
 require 'spec_helper'
 require 'remarkable/mongoid'
 
+# Don't test carrierwave itself. It has a great test suite.
+# Test only that carrierwave is attached and working plus
+# any extras we add -- like saving dimensions.
+
 describe Photo do
   before(:each) do
-    @uploader = Class.new(CarrierWave::Uploader::Base)
     @story = Fabricate(:story)
   end
 
   # Remarkable/mongoid macro
   it { should be_embedded_in(:story) }
 
-  describe "#image" do
-    # Don't test carrierwave itself. It has a great test suite.
-    # Test only that carrierwave is attached and working plus
-    # any extras we add -- like saving dimensions.
-
-    it "when no file is assigned" do
-      @photo = @story.photos.create
-      @photo.image.should be_blank
+  describe ".image" do
+    before(:each) do
+      MiniMagick::Image.stub!(:from_file).and_return(400)
     end
 
-    it "when file is assigned" do
+    it "should fail when no file is assigned" do
+      @photo = @story.photos.build
+      @photo.save.should be_false
+    end
+
+    # This should be enough to test that carrierwave is working
+    it "should succeed when file is assigned" do
       @photo = @story.photos.create :image_filename => "test.jpg"
       @photo.image.current_path.should == (Rails.public_path + "/uploads/photo/image/#{@photo.id}/test.jpg")
+    end
+  end
+
+  describe ".image dimensions" do
+    before(:each) do
+      MiniMagick::Image.should_receive(:from_file).
+                        exactly(2).and_return(:width => 640, :height => 480)
+      @photo = @story.photos.create :image_filename => "test.jpg"
+    end
+
+    it "should save height" do
+      @photo.height.should == 480
+    end
+
+    it "should save width" do
+      @photo.width.should == 640
+    end
+  end
+
+  describe ".image orientation" do
+    it "should save as landscape" do
+      MiniMagick::Image.should_receive(:from_file).
+                        exactly(2).and_return(:width => 640, :height => 480)
+      @photo = @story.photos.create :image_filename => "test.jpg"
+      @photo.orientation.should == "landscape"
+    end
+
+    it "should save as portrait" do
+      MiniMagick::Image.should_receive(:from_file).
+                        exactly(2).and_return(:width => 480, :height => 640)
+      @photo = @story.photos.create :image_filename => "test.jpg"
+      @photo.orientation.should == "portrait"
     end
   end
 end
